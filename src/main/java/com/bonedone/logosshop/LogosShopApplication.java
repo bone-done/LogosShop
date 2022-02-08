ffb832564ae6b7e74f19542a4df5d58b78f0f5fe
Mosklajuk Yurij <dubljanskyj@gmail.com>
2022-02-08 20:32:32 +0200

    init commit

---
+++ src/main/java/com/bonedone/logosshop/LogosShopApplication.java
@@ -0,0 +1,13 @@
+package com.bonedone.logosshop;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LogosShopApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LogosShopApplication.class, args);
+	}
+
+}
